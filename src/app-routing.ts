c6914c81106ca228a74ba7defe751d60f52b5276
olivi3rb <48579174+olivi3rb@users.noreply.github.com>
2021-04-25 00:50:50 -0400

    Add files via upload

--- src/app-routing.ts
+++ src/app-routing.ts
@@ -4,7 +4,7 @@ import Login from "./components/Login.vue";
 import Signup from "./components/Signup.vue";
 import Tasks from "./components/Tasks.vue";
 import Billing from "./components/Billing.vue";
-
+import AddCard from "./components/AddCard.vue";
 Vue.use(VueRouter);
 
 const myRoutes: Array<RouteConfig> = [
@@ -14,20 +14,26 @@ const myRoutes: Array<RouteConfig> = [
     component: Login,
   },
   {
-    name: "signup",
+    name: "Signup",
     path: "/signup",
     component: Signup,
   },
   {
-    name: "tasks",
+    name: "Tasks",
     path: "/tasks",
     component: Tasks,
   },
 
   {
-    name: "billing",
+    name: "Billing",
     path: "/billing",
     component: Billing,
+  },
+
+  {
+    name: "AddCard",
+    path: "/addCard",
+    component: AddCard,
   },
 ];
 
